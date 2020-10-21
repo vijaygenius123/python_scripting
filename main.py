771f0bd288b91b92868b62c40d62311bc936d7b7
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-10-21 22:17:01 +0530

    Added Post Example

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import requests
 
 url = 'http://jsonplaceholder.typicode.com/photos'
 
-resp = requests.get(url)
+#resp = requests.get(url)
+
+#print(resp.json())
+
+jsonPayload  = {"albumId": 1, "title": "Test", "url": "nothing.com", "thumbailUrl": "nothing.com"}
+
+resp = requests.post(url, json= jsonPayload)
 
 print(resp.json())
